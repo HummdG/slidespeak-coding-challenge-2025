4b133fc67a75953efe9917fbd736e81eaf450909
HummdG <hummd2001@gmail.com>
2025-05-26 10:08:45 +0100

    feat: added favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,11 @@ import "./globals.css";
 export const metadata = {
   title: "PowerPoint to PDF Converter - SlideSpeak",
   description: "Convert your PowerPoint File to PDF quickly and efficiently.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 async function RootLayout({ children }: { children: React.ReactNode }) {
